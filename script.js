51c0812fb5b032797b05a7f695a741031d62650a
Muhammad <muhammadsalisuhasaan729@gmail.com>
2023-10-05 12:07:14 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// function load () {
+//     document.querySelector('.holder').classList.add('fade-out')
+// }
+
+// function fadeOut() {
+//     setInterval((load), 3000);
+// }
+
+// window.onload = fadeOut;
+
+<script>
+  AOS.init();
+</script>
